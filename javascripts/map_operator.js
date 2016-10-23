e9aeb03be9681bf0fca97abedd81260ed3541650
webgua <shenxvdong1@gmail.com>
2016-10-24 06:58:09 +0800

    first commit

---
+++ javascripts/map_operator.js
@@ -0,0 +1,366 @@
+var points1 =[
+{'lng':121.482929014647,'lat':31.2406871510026,'count':22,'name':"人民广场-南京东路"},
+{'lng':121.47420215762,'lat':31.2441613462476,'count':18,'name':"人民广场-南京东路1"},
+{'lng':121.47420215762,'lat':31.236873137337,'count':18,'name':"人民广场-南京东路2"},
+{'lng':121.491755002166,'lat':31.2441613462476,'count':18,'name':"人民广场-南京东路3"},
+{'lng':121.491755002166,'lat':31.236873137337,'count':18,'name':"人民广场-南京东路4"},
+
+{'lng':121.446582298406,'lat':31.1999327663924,'count':34,'name':"徐家汇"},
+{'lng':121.442012142544,'lat':31.1953316124699,'count':28,'name':"徐家汇1"},
+{'lng':121.442012142544,'lat':31.2047434798929,'count':28,'name':"徐家汇2"},
+{'lng':121.45244636403,'lat':31.2047434798929,'count':28,'name':"徐家汇3"},
+{'lng':121.45244636403,'lat':31.1953316124699,'count':28,'name':"徐家汇4"},
+
+{'lng':121.520380649463,'lat':31.308721073519,'count':50,'name':"五角场"},
+{'lng':121.512581957609,'lat':31.3012068624108,'count':40,'name':"五角场1"},
+{'lng':121.512581957609,'lat':31.3137428398646,'count':40,'name':"五角场2"},
+{'lng':121.526730973702,'lat':31.3012068624108,'count':40,'name':"五角场3"},
+{'lng':121.526730973702,'lat':31.3137428398646,'count':40,'name':"五角场4"},
+
+{'lng':121.483086477578,'lat':31.2307894219967,'count':31,'name':"淮海路"},
+{'lng':121.478197916118,'lat':31.2273984883647,'count':25,'name':"淮海路1"},
+{'lng':121.478197916118,'lat':31.2335055176991,'count':25,'name':"淮海路2"},
+{'lng':121.487766220992,'lat':31.2273984883647,'count':25,'name':"淮海路3"},
+{'lng':121.487766220992,'lat':31.2335055176991,'count':25,'name':"淮海路4"},
+
+{'lng':121.464640707084,'lat':31.2221177445104,'count':12,'name':"音乐学院"},
+{'lng':121.462056435591,'lat':31.2202245895834,'count':10,'name':"音乐学院1"},
+{'lng':121.462056435591,'lat':31.2243621880036,'count':10,'name':"音乐学院2"},
+{'lng':121.467915956179,'lat':31.2202245895834,'count':10,'name':"音乐学院3"},
+{'lng':121.467915956179,'lat':31.2243621880036,'count':10,'name':"音乐学院4"},
+
+{'lng':121.454375260443,'lat':31.2301644747387,'count':48,'name':"静安寺"},
+{'lng':121.451051176922,'lat':31.227529046715,'count':39,'name':"静安寺1"},
+{'lng':121.451051176922,'lat':31.2324390838576,'count':39,'name':"静安寺2"},
+{'lng':121.457699119604,'lat':31.227529046715,'count':39,'name':"静安寺3"},
+{'lng':121.457699119604,'lat':31.2324390838576,'count':39,'name':"静安寺4"},
+
+
+{'lng':121.462833702931,'lat':31.2351377195479,'count':49,'name':"南京西路"},
+{'lng':121.457265972233,'lat':31.2325294553004,'count':40,'name':"南京西路1"},
+{'lng':121.457265972233,'lat':31.2377071070847,'count':40,'name':"南京西路2"},
+{'lng':121.468442506448,'lat':31.2325294553004,'count':40,'name':"南京西路3"},
+{'lng':121.468442506448,'lat':31.2377071070847,'count':40,'name':"南京西路4"},
+
+{'lng':121.423738218854,'lat':31.2243712953956,'count':33,'name':"中山公园"},
+{'lng':121.419651216845,'lat':31.2220056413452,'count':26,'name':"中山公园1"},
+{'lng':121.419651216845,'lat':31.2261672439938,'count':26,'name':"中山公园2"},
+{'lng':121.42949827096,'lat':31.2220056413452,'count':26,'name':"中山公园3"},
+{'lng':121.42949827096,'lat':31.2261672439938,'count':26,'name':"中山公园4"},
+
+
+{'lng':121.419416230625,'lat':31.2394432657806,'count':67,'name':"环球港"},
+{'lng':121.417071882755,'lat':31.2370242508375,'count':54,'name':"环球港1"},
+{'lng':121.417071882755,'lat':31.2429355474112,'count':54,'name':"环球港2"},
+{'lng':121.421295271167,'lat':31.2370242508375,'count':54,'name':"环球港3"},
+{'lng':121.421295271167,'lat':31.2429355474112,'count':54,'name':"环球港4"},
+
+{'lng':121.460856041605,'lat':31.2511505737355,'count':50,'name':"上海火车站"},
+{'lng':121.455733618491,'lat':31.2483543007763,'count':40,'name':"上海火车站1"},
+{'lng':121.455733618491,'lat':31.2538450596848,'count':40,'name':"上海火车站2"},
+{'lng':121.465492491255,'lat':31.2483543007763,'count':40,'name':"上海火车站3"},
+{'lng':121.465492491255,'lat':31.2538450596848,'count':40,'name':"上海火车站4"},
+
+{'lng':121.485085492792,'lat':31.2766342875169,'count':44,'name':"虹口足球场"},
+{'lng':121.4829389403,'lat':31.2756393166786,'count':35,'name':"虹口足球场1"},
+{'lng':121.4829389403,'lat':31.2779922003563,'count':35,'name':"虹口足球场2"},
+{'lng':121.48747115186,'lat':31.2756393166786,'count':35,'name':"虹口足球场3"},
+{'lng':121.48747115186,'lat':31.2779922003563,'count':35,'name':"虹口足球场4"},
+
+{'lng':121.49098765882,'lat':31.2563443017892,'count':34,'name':"四川北路"},
+{'lng':121.489115554581,'lat':31.2530247802596,'count':27,'name':"四川北路1"},
+{'lng':121.489115554581,'lat':31.2594677669027,'count':27,'name':"四川北路2"},
+{'lng':121.492722983811,'lat':31.2530247802596,'count':27,'name':"四川北路3"},
+{'lng':121.492722983811,'lat':31.2594677669027,'count':27,'name':"四川北路4"},
+
+{'lng':121.47925618411,'lat':31.2495065381322,'count':28,'name':"大悦城"},
+{'lng':121.477581847573,'lat':31.2478976592706,'count':23,'name':"大悦城1"},
+{'lng':121.477581847573,'lat':31.2510499448884,'count':23,'name':"大悦城2"},
+{'lng':121.481074028334,'lat':31.2478976592706,'count':23,'name':"大悦城3"},
+{'lng':121.481074028334,'lat':31.2510499448884,'count':23,'name':"大悦城4"},
+
+{'lng':121.497354724613,'lat':31.2326582363389,'count':47,'name':"城隍庙"},
+{'lng':121.493762346053,'lat':31.2299920694518,'count':38,'name':"城隍庙1"},
+{'lng':121.493762346053,'lat':31.2360202228001,'count':38,'name':"城隍庙2"},
+{'lng':121.500777138604,'lat':31.2299920694518,'count':38,'name':"城隍庙3"},
+{'lng':121.500777138604,'lat':31.2360202228001,'count':38,'name':"城隍庙4"},
+
+{'lng':121.476210713287,'lat':31.2118784389518,'count':11,'name':"打浦桥"},
+{'lng':121.473538034463,'lat':31.2098221480924,'count':9,'name':"打浦桥1"},
+{'lng':121.473538034463,'lat':31.2139347298111,'count':9,'name':"打浦桥2"},
+{'lng':121.478883392111,'lat':31.2098221480924,'count':9,'name':"打浦桥3"},
+{'lng':121.478883392111,'lat':31.2139347298111,'count':9,'name':"打浦桥4"},
+
+{'lng':121.568209314741,'lat':31.2145767255769,'count':22,'name':"花木龙阳路"},
+{'lng':121.562147634598,'lat':31.2094469915016,'count':19,'name':"花木龙阳路1"},
+{'lng':121.562147634598,'lat':31.2204592540529,'count':19,'name':"花木龙阳路2"},
+{'lng':121.572212661326,'lat':31.2094469915016,'count':19,'name':"花木龙阳路3"},
+{'lng':121.572212661326,'lat':31.2204592540529,'count':19,'name':"花木龙阳路4"},
+
+{'lng':121.524366204605,'lat':31.2359142073144,'count':49,'name':"八佰伴"},
+{'lng':121.519860027006,'lat':31.2319694282542,'count':40,'name':"八佰伴1"},
+{'lng':121.519860027006,'lat':31.2397515279293,'count':40,'name':"八佰伴2"},
+{'lng':121.529201587376,'lat':31.2319694282542,'count':40,'name':"八佰伴3"},
+{'lng':121.529201587376,'lat':31.2397515279293,'count':40,'name':"八佰伴4"},
+
+{'lng':121.50727468901,'lat':31.242553835274,'count':9,'name':"陆家嘴"},
+{'lng':121.50414248634,'lat':31.2400880964116,'count':7,'name':"陆家嘴1"},
+{'lng':121.50414248634,'lat':31.2448474373256,'count':7,'name':"陆家嘴2"},
+{'lng':121.510782750137,'lat':31.2400880964116,'count':7,'name':"陆家嘴3"},
+{'lng':121.510782750137,'lat':31.2448474373256,'count':7,'name':"陆家嘴4"},
+
+{'lng':121.585868959354,'lat':31.2629364848406,'count':30,'name':"金桥"},
+{'lng':121.583398631219,'lat':31.2611198495579,'count':24,'name':"金桥1"},
+{'lng':121.583398631219,'lat':31.2647937098459,'count':24,'name':"金桥2"},
+{'lng':121.587794255606,'lat':31.2611198495579,'count':24,'name':"金桥3"},
+{'lng':121.587794255606,'lat':31.2647937098459,'count':24,'name':"金桥4"},
+
+{'lng':121.458447270089,'lat':31.2820343973228,'count':13,'name':"大宁"},
+{'lng':121.455987900958,'lat':31.2786463892415,'count':9,'name':"大宁1"},
+{'lng':121.455987900958,'lat':31.2855162835048,'count':9,'name':"大宁2"},
+{'lng':121.460786393042,'lat':31.2786463892415,'count':9,'name':"大宁3"},
+{'lng':121.460786393042,'lat':31.2855162835048,'count':9,'name':"大宁4"}
+]
+
+
+var styleJson = [
+          {
+                    "featureType": "water",
+                    "elementType": "all",
+                    "stylers": {
+                              "color": "#3aadf7"
+                    }
+          },
+          {
+                    "featureType": "highway",
+                    "elementType": "geometry.fill",
+                    "stylers": {
+                              "color": "#08304a"
+                    }
+          },
+          {
+                    "featureType": "highway",
+                    "elementType": "geometry.stroke",
+                    "stylers": {
+                              "color": "#147a92"
+                    }
+          },
+          {
+                    "featureType": "arterial",
+                    "elementType": "geometry.fill",
+                    "stylers": {
+                              "color": "#0e6095"
+                    }
+          },
+          {
+                    "featureType": "arterial",
+                    "elementType": "geometry.stroke",
+                    "stylers": {
+                              "color": "#08304a"
+                    }
+          },
+          {
+                    "featureType": "local",
+                    "elementType": "geometry",
+                    "stylers": {
+                              "color": "#08304a"
+                    }
+          },
+          {
+                    "featureType": "land",
+                    "elementType": "all",
+                    "stylers": {
+                              "color": "#0e6095"
+                    }
+          },
+          {
+                    "featureType": "railway",
+                    "elementType": "geometry.fill",
+                    "stylers": {
+                              "color": "#000000"
+                    }
+          },
+          {
+                    "featureType": "railway",
+                    "elementType": "geometry.stroke",
+                    "stylers": {
+                              "color": "#08304b"
+                    }
+          },
+          {
+                    "featureType": "subway",
+                    "elementType": "geometry",
+                    "stylers": {
+                              "lightness": -70
+                    }
+          },
+          {
+                    "featureType": "building",
+                    "elementType": "geometry.fill",
+                    "stylers": {
+                              "color": "#000000"
+                    }
+          },
+          {
+                    "featureType": "all",
+                    "elementType": "labels.text.fill",
+                    "stylers": {
+                              "color": "#ffffff"
+                    }
+          },
+          {
+                    "featureType": "all",
+                    "elementType": "labels.text.stroke",
+                    "stylers": {
+                              "color": "#000000"
+                    }
+          },
+          {
+                    "featureType": "building",
+                    "elementType": "geometry",
+                    "stylers": {
+                              "color": "#022338"
+                    }
+          },
+          {
+                    "featureType": "green",
+                    "elementType": "geometry",
+                    "stylers": {
+                              "color": "#2877ae"
+                    }
+          },
+          {
+                    "featureType": "boundary",
+                    "elementType": "all",
+                    "stylers": {
+                              "color": "#1e1c1c"
+                    }
+          },
+          {
+                    "featureType": "manmade",
+                    "elementType": "all",
+                    "stylers": {
+                              "color": "#022338"
+                    }
+          }
+]
+var map1 = new BMap.Map("map1",{enableMapClick:false});
+var point1 = new BMap.Point(121.479,31.249);
+map1.centerAndZoom(point1,13);
+map1.disableScrollWheelZoom();
+map1.disableDoubleClickZoom();
+//设置地图no可以拖拽
+map1.enableDragging();
+var heatmapOverlay1 = new BMapLib.HeatmapOverlay({"radius":35});
+map1.addOverlay(heatmapOverlay1);
+heatmapOverlay1.setDataSet({data:points1,max:50});
+map1.setMapStyle({style:'googlelite'});
+// var marker = new BMap.Marker(new BMap.Point(121.479,31.239));
+// marker.addEventListener("mouseover",attribute);
+// map1.addOverlay(marker);
+
+//添加mouseover事件
+var opts = new Array(20);
+// var infoWindow = new Array(20);
+for(var i=0;i<20;i++){
+     opts[i]={title:'<span style="font-size:14px;color:#fff">'+points1[i*5]['name']+'</span>'};
+     // infoWindow[i] = new BMap.InfoWindow("<div style='line-height:1.8em;font-size:12px;'>123</div>",opts[i]);
+}
+
+for(var i=0;i<20;i++){
+     (function(i){
+          var pt = new BMap.Point(points1[i*5]['lng'], points1[i*5]['lat']);
+          var myIcon = new BMap.Icon("../images/marker.png",new BMap.Size(6,6));
+          var marker2 = new BMap.Marker(pt,{icon:myIcon});  // 创建标注
+          marker2.addEventListener("mouseover",function(){
+               var infoWindow = new BMap.InfoWindow("<div style='line-height:1.8em;font-size:12px;color:#fff;'>"+"<h5>metro_in:"+sodaData1[i]['metro']['in']+"</h5>"+"</div>",opts[i]);
+               this.openInfoWindow(infoWindow);
+               $('.BMap_pop div:nth-child(1) div').css('background-color','rgba(0,0,0,0.5');
+               $('.BMap_pop div:nth-child(3) div').css('background-color','rgba(0,0,0,0.5');
+               $('.BMap_pop div:nth-child(5) div').css('background-color','rgba(0,0,0,0.5');
+               $('.BMap_pop div:nth-child(7) div').css('background-color','rgba(0,0,0,0.5');
+               $('.BMap_pop div:nth-child(8) img').remove();
+          });
+          map1.addOverlay(marker2);
+     })(i);              // 将标注添加到地图中
+}
+map1.setMapStyle({styleJson:styleJson});
+
+
+function attribute(marker){
+	var p = marker.getPosition();  //获取marker的位置
+	alert("marker的位置是" + p.lng + "," + p.lat);    
+}
+
+var nav1_first = false;
+var nav2_first = true;
+var nav3_first = true;
+
+function nav_click1(){
+     $('.li_1').css('background-color','#0E5E94');
+     $('.li_2').css('background-color','#0b4a75');
+     $('.li_3').css('background-color','#0b4a75');
+     $('#map2').css('display','none');
+     $('#map3').css('display','none');
+     $('#map1').css('display','block');
+}
+function nav_click2(){
+     $('.li_2').css('background-color','#0E5E94');
+     $('.li_1').css('background-color','#0b4a75');
+     $('.li_3').css('background-color','#0b4a75');
+     $('#map1').css('display','none');
+     $('#map3').css('display','none');
+     $('#map2').css('display','block');
+     if(nav2_first){
+          var map2 = new BMap.Map("map2");
+          var point2 = new BMap.Point(121.479,31.249);
+          map2.centerAndZoom(point2,13);
+          map2.disableScrollWheelZoom();
+          map2.disableDragging();
+          var heatmapOverlay2 = new BMapLib.HeatmapOverlay({"radius":35});
+          map2.addOverlay(heatmapOverlay2);
+          heatmapOverlay2.setDataSet({data:points1,max:50});
+          map2.setMapStyle({styleJson:styleJson});
+          nav2_first = false;
+     }
+}
+function nav_click3(){
+     $('.li_3').css('background-color','#0E5E94');
+     $('.li_1').css('background-color','#0b4a75');
+     $('.li_2').css('background-color','#0b4a75');
+     $('#map3').css('display','block');
+     $('#map1').css('display','none');
+     $('#map2').css('display','none');
+     if(nav3_first){
+          var map3 = new BMap.Map("map3");
+          var point3 = new BMap.Point(121.479,31.249);
+          map3.centerAndZoom(point3,13);
+          map3.disableScrollWheelZoom();
+          map3.disableDragging();
+          var heatmapOverlay3 = new BMapLib.HeatmapOverlay({"radius":35});
+          map3.addOverlay(heatmapOverlay3);
+          heatmapOverlay3.setDataSet({data:points1,max:50});
+          map3.setMapStyle({styleJson:styleJson});
+          nav3_first = false;
+     }
+}
+
+var sodaData1;
+
+var peoples=[];
+function handle_sodaData(data){
+     sodaData1 = data['people'];
+     // console.log(data['people'])
+     var people = data['people'];
+     for(var i=0;i<20;i++){
+          // console.log(people[i]['metro']);
+          peoples[i] = ((people[i]['metro']['in']+people[i]['metro']['out'])/2+people[i]['unicom']);
+     }
+     $("#bcInfomation").empty();
+     for(var i=0;i<20;i++){
+          console.log(peoples[i]);
+         $("#bcInfomation").append("<h5>"+points1[i*5]['name']+":"+peoples[i]+"</h5>");
+     }
+}
+
+
